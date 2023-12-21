c9d6c11696f4656e101e1c24db26c20e118723a2
k22036 <131113333+k22036@users.noreply.github.com>
2023-12-21 15:26:57 +0900

    ファイルアップロードの処理の追加

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+upload_button = document.getElementById("upload");
+input_file = document.getElementById("file");
+console.log(input_file);
+
+let data = null;
+
+input_file.addEventListener("change", function () {
+    data = input_file.files[0];
+    console.log(data);
+});
+
+upload_button.onclick = function () {
+    let formData = new FormData();
+    formData.append("file", data);
+    
+    $.ajax({
+        url: "/upload",
+        type: "POST",
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        dataType: 'text',
+    })
+    .done(function (response) {
+        console.log(response);
+    })
+}
