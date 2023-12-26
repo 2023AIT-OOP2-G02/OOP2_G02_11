896a309f85a30da0fef3e61a1384d886186eb5e4
MukPan <109339477+MukPan@users.noreply.github.com>
2023-12-27 00:31:31 +0900

    add: yolo以外の画像を正常に生成できるように

--- model/watch_dir.py
+++ model/watch_dir.py
@@ -2,25 +2,38 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+from model.face_detect_rectangle import detect_face
+from model.face_mosaic import face_mosaic
+from model.gray_scale import gray_scale
+from model.outline_extraction import outline_extract
+
 
 # ディレクトリを監視
 class DirWatcher(FileSystemEventHandler):
     def on_created(self, event):
-        if not event.is_directory:
-            # ファイル名のみを取得
-            file_name = event.src_path.split("/")[-1]
-            print(f"ファイルが作成されました。: {file_name}")
-
-            # 画像処理
-
-
-
-
-
-
-        else:
+        # ディレクトリは弾く
+        if event.is_directory:
             print(f"ディレクトリが作成されました。: {event.src_path}")
+            return
 
+        # ファイル名のみを取得
+        file_name = event.src_path.split("\\")[-1]
+        print(f"ファイルが作成されました。: {file_name}")
+
+        # 画像処理
+        # 処理後はimage/outputに保存
+        try:
+            detect_face(file_name)
+            face_mosaic(file_name)
+            gray_scale(file_name)
+            outline_extract(file_name)
+            # modelpicture(file_name)
+        except Exception as e:
+            print(e)
+            print("画像処理に失敗しました。")
+            return
+
+        print("画像処理が完了しました。")
 
 # ディレクトリ監視を開始する関数
 def start_watchdog(directory_path):
