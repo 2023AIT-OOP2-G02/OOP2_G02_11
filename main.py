6a3790d8d4b2f18277e670c754a2e5e6e200cefd
k22036 <131113333+k22036@users.noreply.github.com>
2023-12-27 09:51:00 +0900

    [add] remove_imgを追加 （未実装）

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json  # Python標準のJSONライブラリを読み込んで、データ�
 
 from model.watch_dir import start_watchdog
 
+
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
 
---
+++ remove_img.py
@@ -0,0 +1,1 @@
+# input と　output のimgを削除する
